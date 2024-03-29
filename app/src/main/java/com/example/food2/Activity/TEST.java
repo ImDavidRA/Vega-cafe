863be099f902342de42febe55f2dc9cabb81e6a2
DavidRA <david.romero.araez@ces-vegamedia.es>
2024-03-29 11:21:42 +0100

    Video finalizado, carrito y esas cosas puestas

---
+++ app/src/main/java/com/example/food2/Activity/TEST.java
@@ -0,0 +1,114 @@
+package com.example.food2.Activity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.etebarian.meowbottomnavigation.MeowBottomNavigation;
+import com.example.food2.R;
+import com.example.food2.TestFragments.CartFragment;
+import com.example.food2.TestFragments.HomeFragment;
+import com.example.food2.TestFragments.ProfileFragment;
+import com.example.food2.TestFragments.SettingsFragment;
+
+import kotlin.Unit;
+import kotlin.jvm.functions.Function1;
+
+public class TEST extends AppCompatActivity {
+
+    MeowBottomNavigation bottomNavigation;
+    RelativeLayout main_layout;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+
+
+        EdgeToEdge.enable(this);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main_layout), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        main_layout = findViewById(R.id.main_layout);
+
+        replace(new HomeFragment());
+        main_layout.setBackgroundColor(Color.parseColor("#FF5722"));
+
+        // Creamos objeto con el id bottomNavigation
+        bottomNavigation = findViewById(R.id.bottomNavigation);
+
+        // Default en el que iniciará
+        bottomNavigation.show(1,true);
+
+        // Añadimos la cantidad de elementos que queremos que tenga la barra inferior
+        bottomNavigation.add(new MeowBottomNavigation.Model(1,R.drawable.baseline_home_24));
+        bottomNavigation.add(new MeowBottomNavigation.Model(2,R.drawable.baseline_shopping_cart_24));
+        bottomNavigation.add(new MeowBottomNavigation.Model(3,R.drawable.baseline_person_24));
+        bottomNavigation.add(new MeowBottomNavigation.Model(4,R.drawable.baseline_settings_24));
+
+        meowNavigation();
+
+    }
+
+
+    private void meowNavigation() {
+
+        bottomNavigation.setOnClickMenuListener(new Function1<MeowBottomNavigation.Model, Unit>() {
+            @Override
+            public Unit invoke(MeowBottomNavigation.Model model) {
+
+                switch (model.getId()) {
+
+                    case 1:
+
+                        replace(new HomeFragment());
+                        main_layout.setBackgroundColor(Color.parseColor("#FF5722"));
+
+                        break;
+
+                    case 2:
+
+                        replace(new CartFragment());
+                        main_layout.setBackgroundColor(Color.parseColor("#009688"));
+
+                        break;
+
+                    case 3:
+
+                        replace(new ProfileFragment());
+                        main_layout.setBackgroundColor(Color.parseColor("#FFDE59"));
+
+                        break;
+
+                    case 4:
+
+                        replace(new SettingsFragment());
+                        main_layout.setBackgroundColor(Color.parseColor("#03A9F4"));
+
+                        break;
+                }
+
+                return null;
+            }
+        });
+
+    } ////////////////////
+
+    private void replace(Fragment f) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frameLayout, f);
+        transaction.commit();
+    }
+
+}
