2c889f864f944b685696648f6b3641e42adf7bb2
davidra2507 <david.romero.araez@ces-vegamedia.es>
2024-04-15 14:34:26 +0200

    fjksfjsifjsldikf

--- app/src/main/java/com/example/food2/Activity/AlmacenActivity.java
+++ app/src/main/java/com/example/food2/Activity/AlmacenActivity.java
@@ -4,24 +4,37 @@ import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
-import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.food2.Adapter.FoodListAdapter;
+import com.example.food2.Domain.Foods;
 import com.example.food2.R;
 import com.example.food2.databinding.ActivityAlmacenBinding;
-import com.example.food2.databinding.ActivityDetailBinding;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
-public class AlmacenActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class AlmacenActivity extends BaseActivity {
 
     ActivityAlmacenBinding binding;
+    private boolean isSearch;
+    String text;
+    private String searchText;
+    private RecyclerView.Adapter adapterListFood;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         binding = ActivityAlmacenBinding.inflate(getLayoutInflater());
@@ -56,7 +69,10 @@ public class AlmacenActivity extends AppCompatActivity {
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                binding.emptyTxt.setText("CAMBIADO");
+                text = binding.searchEdt.getText().toString();
+                searchText = text;
+
+                initList();
             }
 
             @Override
@@ -66,6 +82,46 @@ public class AlmacenActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void initList() {
+        DatabaseReference myRef = database.getReference("Foods");
+        binding.emptyTxt.setVisibility(View.VISIBLE);
+        ArrayList<Foods> list = new ArrayList<>();
+
+        Query query;
+        if(isSearch) {
+            String sub = searchText.substring(0,1).toUpperCase();
+            searchText = searchText.substring(1);
+
+            searchText = sub+searchText;
+
+            query = myRef.orderByChild("Title").startAt(searchText).endAt(searchText+'\uf8ff');
+        } else {
+            query = myRef.orderByChild("Title").startAt(searchText).endAt(searchText+'\uf8ff');
+        }
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists()){
+                    for(DataSnapshot issue: snapshot.getChildren()) {
+                        list.add(issue.getValue(Foods.class));
+                    }
+                    if(list.size()>0) {
+                        binding.foodListView.setLayoutManager(new GridLayoutManager(AlmacenActivity.this, 1));
+                        adapterListFood = new FoodListAdapter(list);
+                        binding.foodListView.setAdapter(adapterListFood);
+                        binding.foodListView.setAdapter(adapterListFood);
+                    }
+                    binding.emptyTxt.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
 }
